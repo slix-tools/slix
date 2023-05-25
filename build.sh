b402e6f3410e93b0f665204a92df01b2a910f5d6
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-26 00:17:21 +0200

    move fuse-union around

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
 mkdir -p obj
-FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64 -isystem microtar/src"
+FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64"
 FLAGS="${FLAGS} -ggdb"
 
-g++ ${FLAGS} -c main.cpp -o obj/main.cpp.o
-g++ obj/main.cpp.o -lfuse -larchive -o fuse-union
+g++ ${FLAGS} -c fuse-union.cpp -o obj/fuse-union.cpp.o
+g++ obj/fuse-union.cpp.o -lfuse -larchive -o fuse-union
 
 g++ ${FLAGS} -c archive.cpp -o obj/archive.cpp.o
 g++ obj/archive.cpp.o -lfuse -larchive -o archive
