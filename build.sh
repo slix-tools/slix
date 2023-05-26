fd351ffc2e585fe6341e6f4ed675acd39d05c1db
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-26 21:45:38 +0200

    patch: put source files into own folder

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ mkdir -p obj
 FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64"
 FLAGS="${FLAGS} -ggdb"
 
-g++ ${FLAGS} -c fuse-union.cpp -o obj/fuse-union.cpp.o
+g++ ${FLAGS} -c src/fuse-union.cpp -o obj/fuse-union.cpp.o -isystem src
 g++ obj/fuse-union.cpp.o -lfuse -larchive -o fuse-union
 
-g++ ${FLAGS} -c archive.cpp -o obj/archive.cpp.o
+g++ ${FLAGS} -c src/archive.cpp -o obj/archive.cpp.o -isystem src
 g++ obj/archive.cpp.o -lfuse -larchive -o archive
