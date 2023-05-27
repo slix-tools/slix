b61dae20cb1466abfd83a8ce1e558f5f158af275
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-27 21:14:42 +0200

    patch: remove all FileFuse related code

--- src/slix-shell.cpp
+++ src/slix-shell.cpp
@@ -40,325 +40,6 @@ auto create_temp_dir() -> std::filesystem::path {
     throw std::runtime_error{"failed creating temporary directory"};
 }
 
-struct Node {
-    std::unordered_map<std::string, Node> children;
-
-    auto findPath(std::filesystem::path::iterator iter, std::filesystem::path::iterator end) const -> Node const* {
-        ++iter;
-        if (iter == end) return this;
-        if (auto child_iter = children.find(*iter); child_iter != children.end()) {
-            return child_iter->second.findPath(iter, end);
-        }
-        return nullptr;
-    }
-    auto findPath(std::filesystem::path const &path) const -> Node const* {
-        return findPath(path.begin(), path.end());
-    }
-
-    auto findPath(std::filesystem::path::iterator iter, std::filesystem::path::iterator end) -> Node* {
-        ++iter;
-        if (iter == end) return this;
-        if (auto child_iter = children.find(*iter); child_iter != children.end()) {
-            return child_iter->second.findPath(iter, end);
-        }
-        return nullptr;
-    }
-    auto findPath(std::filesystem::path const &path) -> Node* {
-        return findPath(path.begin(), path.end());
-    }
-
-};
-
-auto scanFolder(std::filesystem::path const& _path) -> Node {
-    //std::cout << "scanning: " << _path << "\n";
-    auto node = Node{};
-    for (auto const& dir_entry : std::filesystem::directory_iterator{_path, std::filesystem::directory_options::skip_permission_denied}) {
-        auto name = dir_entry.path().filename().string();
-        if (name == "." || name == "..") continue;
-        if (dir_entry.is_directory() && !dir_entry.is_symlink()) {
-            auto children = scanFolder(_path / name);
-            node.children.try_emplace(std::move(name), std::move(children));
-        } else if (dir_entry.is_regular_file()) {
-            //std::cout << "adding file: " << _path / name << "\n";
-            node.children.try_emplace(std::move(name));
-        } else if (dir_entry.is_symlink()) {
-            node.children.try_emplace(std::move(name));
-        } else {
-            //std::cout << "unsupported, skipping: " << _path / name << "\n";
-        }
-        //std::cout << dir_entry.path() << '\n';
-    }
-    return node;
-}
-
-struct FileFuse {
-    std::filesystem::path rootPath;
-    Node tree;
-    bool writable{false};
-
-    FileFuse(std::filesystem::path rootPath_, bool writable_)
-        : rootPath{std::move(rootPath_)}
-        , tree{scanFolder(rootPath)}
-        , writable{writable_}
-    {}
-    FileFuse(FileFuse&&) noexcept = default;
-
-    auto real_path(std::filesystem::path const& path) const -> std::filesystem::path {
-        auto realPath = rootPath / path.lexically_proximate("/");
-        return realPath;
-    }
-
-    int getattr_callback(char const* path, struct stat* stbuf) {
-        auto node = tree.findPath(path);
-        //std::cout << "ff, getattr: " << path << " -> " << real_path(path) << " " << (bool)node << "\n";
-        if (!node) return -ENOENT;
-
-        auto r = lstat(real_path(path).c_str(), stbuf);
-        return r;
-    }
-
-    int readlink_callback(char const* path, char* targetBuf, size_t size) {
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        auto realPath = real_path(path);
-
-        if (!std::filesystem::is_symlink(realPath)) return -ENOENT;
-
-        auto linkPath = std::filesystem::read_symlink(realPath).string();
-        size = std::min(size-1, linkPath.size());
-        std::copy(linkPath.c_str(), linkPath.c_str()+size, targetBuf);
-        targetBuf[size] = '\0';
-        //std::cout << "read link: " << linkPath << " " << targetBuf << "\n";
-        return 0;
-    }
-    int mknod_callback(char const* path, mode_t m, dev_t d) {
-        if (!writable) return -ENOENT;
-        //std::cout << "mknod for " << path << "\n";
-
-        auto path_ = std::filesystem::path{path};
-        auto parent = path_.parent_path();
-
-        if (!std::filesystem::is_directory(real_path(parent))) {
-            auto dm = m;
-            if (dm | S_IRWXO) dm |= S_IXOTH;
-            if (dm | S_IRWXG) dm |= S_IXGRP;
-            if (dm | S_IRWXU) dm |= S_IXUSR;
-            auto r = mkdir_callback(parent.c_str(), dm);
-            if (r == -ENOENT) return -ENOENT;
-        }
-        auto node = tree.findPath(parent);
-        node->children.try_emplace(path_.filename().string());
-        return mknod(real_path(path).c_str(), m, d);
-    }
-    int mkdir_callback(char const* path, mode_t m) {
-        if (!writable) return -ENOENT;
-        //std::cout << "mkdir for " << path << "\n";
-
-        auto path_ = std::filesystem::path{path};
-        auto parent = path_.parent_path();
-        if (!std::filesystem::is_directory(real_path(parent))) {
-            auto r = mkdir_callback(parent.c_str(), m);
-            if (r == -ENOENT) return -ENOENT;
-        }
-        auto node = tree.findPath(parent);
-
-        node->children.try_emplace(path_.filename().string());
-        //std::cout << "creating " << path << " at " << real_path(path) << "\n";
-        return mkdir(real_path(path).c_str(), m);
-    }
-    int unlink_callback(char const* path) {
-        if (!writable) return -ENOENT;
-        //std::cout << "unlink for " << path << "\n";
-
-        auto path_ = std::filesystem::path{path};
-
-        auto pn = tree.findPath(path_.parent_path());
-        if (!pn) return -ENOENT;
-        auto r = unlink(real_path(path_).c_str());
-        if (r == 0) {
-            pn->children.erase(path_.filename().string());
-        }
-        return r;
-    }
-    int rmdir_callback(char const* path) {
-        if (!writable) return -ENOENT;
-        //std::cout << "rmdir for " << path << "\n";
-
-        auto path_ = std::filesystem::path{path};
-
-        auto pn = tree.findPath(path_.parent_path());
-        if (!pn) return -ENOENT;
-        auto r = rmdir(real_path(path_).c_str());
-        if (r == 0) {
-            pn->children.erase(path_.filename().string());
-        }
-        return r;
-    }
-
-    int symlink_callback(char const* path, char const* target) {
-        if (!writable) return -ENOENT;
-        //std::cout << "symlink for " << path << " " << target << "\n";
-
-        auto path_   = std::filesystem::path{path};
-        auto target_ = std::filesystem::path{target};
-        auto parent  = target_.parent_path();
-
-        if (!std::filesystem::is_directory(real_path(parent))) {
-            auto dm = S_IXOTH | S_IROTH |
-                      S_IXGRP | S_IRGRP |
-                      S_IRWXU;
-            auto r = mkdir_callback(parent.c_str(), dm);
-            if (r == -ENOENT) return -ENOENT;
-        }
-
-        auto node = tree.findPath(parent);
-        if (!node) return -ENOENT;
-
-
-        node->children.try_emplace(target_.filename().string());
-        return symlink(path, real_path(target).c_str());
-    }
-    int rename_callback(char const* path, char const* target) {
-        if (!writable) return -ENOENT;
-        //std::cout << "rename " << path << " -> " << target << "\n";
-
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        auto path_ = std::filesystem::path{path};
-        auto target_ = std::filesystem::path{target};
-
-        auto pn = tree.findPath(path_.parent_path());
-        if (!pn) return -ENOENT;
-
-        auto tpn = tree.findPath(target_.parent_path()); // missing dirs must be created
-        if (!tpn) return -ENOENT;
-
-        auto r = rename(real_path(path).c_str(), real_path(target).c_str());
-
-        //std::cout << "real move: " << real_path(path) << " -> " << real_path(target) << "\n";
-
-        //std::cout << "parents: " << path_.parent_path() << " -> " << target_.parent_path() << "\n";
-        //std::cout << "filename: " << path_.filename() << " -> " << target_.filename() << "\n";
-        // we lost the original name
-        auto en = pn->children.at(path_.filename());
-        pn->children.erase(path_.filename());
-        tpn->children[target_.filename()] = std::move(en);
-        return r;
-    }
-    int chmod_callback(char const* path, mode_t m) {
-        if (!writable) return -ENOENT;
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-        return chmod(real_path(path).c_str(), m);
-    }
-    int chown_callback(char const* path, uid_t uid, gid_t gid) {
-        if (!writable) return -ENOENT;
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-        return chown(path, uid, gid);
-    }
-
-    int open_callback(char const* path, fuse_file_info* fi) {
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        if (!writable) {
-            if (fi->flags & O_WRONLY || fi->flags & O_RDWR) {
-                return -ENOENT;
-            }
-        }
-
-        auto path_ = std::filesystem::path{path};
-        if (fi->flags & O_WRONLY || fi->flags & O_RDWR) {
-            auto parent = path_.parent_path();
-
-            if (!std::filesystem::is_directory(real_path(parent))) {
-                auto dm = S_IXOTH | S_IROTH |
-                          S_IXGRP | S_IRGRP |
-                          S_IRWXU;
-                auto r = mkdir_callback(parent.c_str(), dm);
-                if (r == -ENOENT) return -ENOENT;
-            }
-        }
-
-        auto fd = open(real_path(path_).c_str(), fi->flags);
-        fi->fh = fd;
-        //std::cout << "open " << path_ << "\n";
-        return 0;
-    }
-
-    int read_callback(char const* path, char* buf, size_t size, off_t offset, fuse_file_info* fi) {
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        auto dataread = pread(fi->fh, buf, size, offset);
-        //std::cout << "read " << path << " " << size << " " << dataread << " " << offset << "\n";
-        return dataread;
-    }
-    int write_callback(char const* path, char const* buf, size_t size, off_t offset, fuse_file_info* fi) {
-        //std::cout << "trying to write\n";
-        if (!writable) {
-            return -ENOENT;
-        }
-        return pwrite(fi->fh, buf, size, offset);
-    }
-    int statfs_callback(char const* path, struct statvfs* fs) {
-        //std::cout << "### reporting statfs for: " << path << "\n";
-        return statvfs(rootPath.c_str(), fs);
-    }
-
-    int release_callback(char const* path, fuse_file_info* fi) {
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-        //std::cout << "release for " << path << "\n";
-
-        close(fi->fh);
-        return 0;
-    }
-
-    int readdir_callback(char const* path, void* buf, fuse_fill_dir_t filler, std::unordered_set<std::string>& satisfiedFiles) {
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        //std::cout << "listing " << path << "\n";
-        for (auto const& [child_name, child_node] : node->children) {
-            //std::cout << "comparing with: " << child_name << "\n";
-
-            if (!satisfiedFiles.contains(child_name)) {
-                //std::cout << " - child: " << child_name << "\n";
-
-                filler(buf, child_name.c_str(), nullptr, 0);
-                satisfiedFiles.emplace(child_name);
-            }
-        }
-        //std::cout << "trying to read " << path << "\n";
-        return 0;
-    }
-    int lock_callback(char const* path, fuse_file_info* fi, int cmd, flock* l) {
-        if (!writable) return -ENOENT;
-        //std::cout << "lock for " << path << " cmd: ";
-        //if (cmd == F_GETLK) std::cout << "F_GETLK";
-        //if (cmd == F_SETLK) std::cout << "F_SETLK";
-        //if (cmd == F_SETLKW) std::cout << "F_SETLKW";
-        //std::cout << "\n";
-
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-
-        return fcntl(fi->fh, cmd);
-    }
-
-    int utimens_callback(char const* path, struct timespec const tv[2]) {
-        //std::cout << "trying to set time for: " << path << "\n";
-        if (!writable) return -ENOENT;
-        auto node = tree.findPath(path);
-        if (!node) return -ENOENT;
-        return utimensat(AT_FDCWD, real_path(path).c_str(), tv, 0);
-    }
-};
-
 struct GarFuse {
     std::filesystem::path rootPath;
 
@@ -519,9 +200,9 @@ struct MyFuse {
     fuse*      fusePtr {nullptr};
     std::filesystem::path mountPoint;
 
-    std::vector<std::variant<FileFuse, GarFuse>> nodes;
+    std::vector<GarFuse> nodes;
 
-    MyFuse(std::vector<std::variant<FileFuse, GarFuse>> nodes_, std::vector<std::string> extraOptions)
+    MyFuse(std::vector<GarFuse> nodes_, std::vector<std::string> extraOptions)
         : mountPoint{create_temp_dir()}
         , nodes{std::move(nodes_)} {
         std::cout << "creating mount point at " << mountPoint << "\n";
@@ -593,9 +274,7 @@ struct MyFuse {
     template <typename CB>
     static int any_callback(CB && cb) {
         for (auto& fs : self().nodes) {
-            auto r = std::visit([&](auto& fs) {
-                return cb(fs);
-            }, fs);
+            auto r = cb(fs);
             if (r != -ENOENT) return r;
         }
         return -ENOENT;
@@ -631,9 +310,7 @@ struct MyFuse {
     int readdir_callback(char const* path, void* buf, fuse_fill_dir_t filler) {
         auto satisfiedFiles = std::unordered_set<std::string>{};
         for (auto& fs : nodes) {
-            std::visit([&](auto& fs) {
-                fs.readdir_callback(path, buf, filler, satisfiedFiles);
-            }, fs);
+            fs.readdir_callback(path, buf, filler, satisfiedFiles);
         }
 //        std::cout << "reporting files:\n";
 //        for (auto s : satisfiedFiles) {
@@ -652,7 +329,7 @@ int main(int argc, char** argv) {
         throw std::runtime_error{"need at least 1 parameters"};
     }
     auto input = std::vector<std::filesystem::path>{};
-    auto layers = std::vector<std::variant<FileFuse, GarFuse>>{};
+    auto layers = std::vector<GarFuse>{};
     auto packages = std::vector<std::string>{};
     auto addedPackages = std::unordered_set<std::string>{};
     for (size_t i{1}; i < argc; ++i) {
@@ -664,14 +341,9 @@ int main(int argc, char** argv) {
         if (addedPackages.contains(input)) continue;
         addedPackages.insert(input);
         std::cout << "layer " << layers.size() << " - ";
-        if (input.extension() == ".gar") {
-            layers.emplace_back<GarFuse>(input);
-            auto const& fuse = std::get<GarFuse>(layers.back());
-            for (auto const& d : fuse.dependencies) {
-                packages.push_back(d);
-            }
-        } else {
-            layers.emplace_back(FileFuse{input, false});
+        auto const& fuse = layers.emplace_back(input);
+        for (auto const& d : fuse.dependencies) {
+            packages.push_back(d);
         }
     }
 
