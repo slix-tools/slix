c2e11b9cab7774301e8ec1477b3f0cc309a7109f
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-29 11:54:17 +0200

    fix: removing linkage against libarchive, wasn't used anyways

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ FLAGS="${FLAGS} -ggdb"
 g++ ${FLAGS} -c src/InteractiveProcess.cpp -o obj/InteractiveProcess.o
 
 g++ ${FLAGS} -c src/slix-shell.cpp -o obj/slix-shell.cpp.o
-g++ obj/slix-shell.cpp.o obj/InteractiveProcess.o -lfuse -larchive -o slix-shell
+g++ obj/slix-shell.cpp.o obj/InteractiveProcess.o -lfuse -o slix-shell
 
 g++ ${FLAGS} -c src/archive.cpp -o obj/archive.cpp.o
-g++ obj/archive.cpp.o -lfuse -larchive -o archive
+g++ obj/archive.cpp.o -lfuse -o archive
