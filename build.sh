16a42155ba127d3d27ba2a069c22374d32390810
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-05 23:12:03 +0200

    move compilation into build folder

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir -p obj
+mkdir -p build/{obj,bin}
 FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64"
 FLAGS="${FLAGS} -ggdb -O0"
 
-g++ ${FLAGS} -c src/InteractiveProcess.cpp -o obj/InteractiveProcess.o
+g++ ${FLAGS} -c src/InteractiveProcess.cpp -o build/obj/InteractiveProcess.o
 
-g++ ${FLAGS} -c src/slix-shell.cpp -o obj/slix-shell.cpp.o
-g++ obj/slix-shell.cpp.o obj/InteractiveProcess.o -lfuse -o slix-shell
+g++ ${FLAGS} -c src/slix-shell.cpp -o build/obj/slix-shell.cpp.o
+g++ build/obj/slix-shell.cpp.o build/obj/InteractiveProcess.o -lfuse -o build/bin/slix-shell
 
-g++ ${FLAGS} -c src/slix-ld.cpp -o obj/slix-ld.cpp.o -static
-g++ obj/slix-ld.cpp.o -o slix-ld -static
+g++ ${FLAGS} -c src/slix-ld.cpp -o build/obj/slix-ld.cpp.o -static
+g++ build/obj/slix-ld.cpp.o -o build/bin/slix-ld -static
 
-g++ ${FLAGS} -c src/archive.cpp -o obj/archive.cpp.o
-g++ obj/archive.cpp.o -lfuse -o archive
+g++ ${FLAGS} -c src/archive.cpp -o build/obj/archive.cpp.o
+g++ build/obj/archive.cpp.o -lfuse -o build/bin/archive
 
-g++ ${FLAGS} -c src/packageAnalyzer.cpp -o obj/packageAnalyzer.cpp.o
-g++ obj/packageAnalyzer.cpp.o -lfuse -o packageAnalyzer
+g++ ${FLAGS} -c src/packageAnalyzer.cpp -o build/obj/packageAnalyzer.cpp.o
+g++ build/obj/packageAnalyzer.cpp.o -lfuse -o build/bin/packageAnalyzer
--- create-slix-ld.sh
+++ create-slix-ld.sh
@@ -2,8 +2,9 @@
 
 
 ./build.sh
-cp slix-ld slix-ld-package/rootfs/usr/bin
-./archive slix-ld-package
+mkdir -p slix-ld-package/rootfs/usr/bin
+cp build/bin/slix-ld slix-ld-package/rootfs/usr/bin
+./build/bin/archive slix-ld-package
 
 hash=$(sha256sum -b slix-ld-package.gar | awk '{print $1}')
 mv slix-ld-package.gar arch-packages/slix-ld@1.0.0-${hash}.gar
