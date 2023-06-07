89e3ebb1fec074f65fb2af6a7955c7af336c67ad
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-07 10:37:01 +0200

    feat: add clice as command line parsing

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 mkdir -p build/{obj,bin}
-FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64"
+FLAGS="-std=c++20 -D_FILE_OFFSET_BITS=64 -isystem libs/clice/src"
 FLAGS="${FLAGS} -ggdb -O0"
 
 g++ ${FLAGS} -c src/InteractiveProcess.cpp -o build/obj/InteractiveProcess.o
 
 g++ ${FLAGS} -c src/slix-shell.cpp -o build/obj/slix-shell.cpp.o
-g++ build/obj/slix-shell.cpp.o build/obj/InteractiveProcess.o -lfuse -o build/bin/slix-shell
+g++ build/obj/slix-shell.cpp.o build/obj/InteractiveProcess.o -lfuse -lfmt -o build/bin/slix-shell
 
 g++ ${FLAGS} -c src/slix-ld.cpp -o build/obj/slix-ld.cpp.o -static
 g++ build/obj/slix-ld.cpp.o -o build/bin/slix-ld -static
--- src/slix-shell.cpp
+++ src/slix-shell.cpp
@@ -4,6 +4,7 @@
 #include "InteractiveProcess.h"
 
 #include <atomic>
+#include <clice/clice.h>
 #include <csignal>
 #include <filesystem>
 #include <functional>
@@ -11,6 +12,26 @@
 #include <memory>
 #include <sstream>
 #include <thread>
+
+namespace {
+auto cliHelp = clice::Argument { .arg      = {"--help"},
+                                 .desc     = "prints the help page",
+                                 .cb       = []{ std::cout << clice::generateHelp(); exit(0); },
+};
+auto cliCommand = clice::Argument { .arg   = {"-c"},
+                                    .desc  = "program to execute inside the shell",
+                                    .value = std::string{"bash"},
+};
+
+auto cliVerbose = clice::Argument{ .arg    = {"--verbose"},
+                                   .desc   = "detailed description of what is happening",
+};
+
+auto cliPackages = clice::Argument{ .arg   = "-p",
+                                    .desc  = "packages to initiate inside the environment",
+                                    .value = std::vector<std::string>{},
+};
+}
 
 
 std::atomic_bool finish{false};
@@ -27,26 +48,22 @@ auto searchPackagePath(std::vector<std::filesystem::path> const& slixRoots, std:
     return name;
 }
 
-int main(int argc, char** argv) {
-    if (argc < 1) {
-        throw std::runtime_error{"need at least 1 parameters"};
+auto getSlixRoots() -> std::vector<std::filesystem::path> {
+    auto ptr = std::getenv("SLIX_ROOT");
+    if (!ptr) throw std::runtime_error("unknown SLIX_ROOT");
+    auto paths = std::vector<std::filesystem::path>{};
+    for (auto part : std::views::split(std::string_view{ptr}, ':')) {
+        paths.emplace_back(std::string_view{part.begin(), part.size()});
     }
-    auto slixRoots = [&]() -> std::vector<std::filesystem::path> {
-        auto ptr = std::getenv("SLIX_ROOT");
-        if (!ptr) throw std::runtime_error("unknown SLIX_ROOT");
-        auto paths = std::vector<std::filesystem::path>{};
-        for (auto part : std::views::split(std::string_view{ptr}, ':')) {
-            paths.emplace_back(std::string_view{part.begin(), part.size()});
-        }
-        return paths;
-    }();
+    return paths;
+}
+
+void app() {
+    auto slixRoots = getSlixRoots();
     auto input = std::vector<std::filesystem::path>{};
     auto layers = std::vector<GarFuse>{};
-    auto packages = std::vector<std::string>{};
+    auto packages = *cliPackages;
     auto addedPackages = std::unordered_set<std::string>{};
-    for (size_t i{1}; i < argc; ++i) {
-        packages.emplace_back(argv[i]);
-    }
     while (!packages.empty()) {
         auto input = std::filesystem::path{packages.back()};
         packages.pop_back();
@@ -77,7 +94,8 @@ int main(int argc, char** argv) {
         envp.push_back("PATH=" + fuseFS.mountPoint.string() + "/usr/bin");
         envp.push_back("LD_LIBRARY_PATH=" + fuseFS.mountPoint.string() + "/usr/lib");
 
-        auto ip = process::InteractiveProcess{std::vector<std::string>{fuseFS.mountPoint.string() + "/usr/bin/bash", "--norc", "--noprofile"}, envp};
+        auto ip = process::InteractiveProcess{std::vector<std::string>{"/usr/bin/env", *cliCommand}, envp};
+
         onExit = [&](int signal) {
             ip.sendSignal(signal);
         };
@@ -88,4 +106,20 @@ int main(int argc, char** argv) {
     finish = true;
     fuseFS.close();
     runningFuse.join();
+
+}
+
+int main(int argc, char** argv) {
+    try {
+        if (auto failed = clice::parse(argc, argv); failed) {
+            std::cerr << "parsing failed: " << *failed << "\n";
+            return 1;
+        }
+        if (auto ptr = std::getenv("CLICE_COMPLETION"); ptr) {
+            return 0;
+        }
+        app();
+    } catch (std::exception const& e) {
+        std::cerr << "error: " << e.what() << "\n";
+    }
 }
