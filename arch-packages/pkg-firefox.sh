2e0b8365afa872fd9e0295ff6780fc7aa364b91a
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-10 22:32:10 +0200

    fix: firefox needs to overwrite default script

--- arch-packages/pkg-firefox.sh
+++ arch-packages/pkg-firefox.sh
@@ -1,3 +1,6 @@
 name=firefox
-deps="slix-ld dbus-glib ffmpeg gtk3 libpulse libxt mime-types nss gnu-free-fonts sed"
-source pkg.sh
+deps="slix-ld dbus-glib ffmpeg gtk3 libpulse libxt mime-types nss gnu-free-fonts sed bash"
+./preparePackage.sh "${name}" "${deps}"
+
+cp --remove-destination data/firefox/firefox ${name}/rootfs/usr/bin/firefox
+./finalizePackage.sh "${name}"
