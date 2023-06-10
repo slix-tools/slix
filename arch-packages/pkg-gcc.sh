f4e612b57ee0c4376932a3c0a752f08896594024
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-10 22:27:55 +0200

    pkg: clean up gcc creation

--- arch-packages/pkg-gcc.sh
+++ arch-packages/pkg-gcc.sh
@@ -3,18 +3,7 @@ deps="slix-ld gcc-libs binutils libmpc zstd libisl"
 
 ./preparePackage.sh "${name}" "${deps}"
 
-rm ${name}/rootfs/usr/bin/gcc
-(
-    echo "#!/usr/bin/env bash"
-    echo "slix-ld-gcc --sysroot \${SLIX_ROOT} \"\$@\""
-) > ${name}/rootfs/usr/bin/gcc
-chmod +x ${name}/rootfs/usr/bin/gcc
-
-rm ${name}/rootfs/usr/bin/g++
-(
-    echo "#!/usr/bin/env bash"
-    echo "slix-ld-g++ --sysroot \${SLIX_ROOT} \"\$@\""
-) > ${name}/rootfs/usr/bin/g++
-chmod +x ${name}/rootfs/usr/bin/g++
+cp --remove-destination data/gcc/gcc ${name}/rootfs/usr/bin/gcc
+cp --remove-destination data/gcc/g++ ${name}/rootfs/usr/bin/g++
 
 ./finalizePackage.sh "${name}"
