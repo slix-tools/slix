00fd907fe462cd1d3c238d0a9350ef3396e226c3
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-17 19:15:56 +0200

    fix gcc13

--- arch-packages/pkg-gcc13.sh
+++ arch-packages/pkg-gcc13.sh
@@ -4,7 +4,7 @@ deps="slix-ld gcc-libs binutils libmpc zstd libisl"
 
 ./preparePackage.sh "${name}" "${archpkg}" "${deps}"
 
-cp --remove-destination data/gcc/gcc ${name}/rootfs/usr/bin/gcc
-cp --remove-destination data/gcc/g++ ${name}/rootfs/usr/bin/g++
+cp -a --remove-destination data/gcc/gcc ${name}/rootfs/usr/bin/gcc
+cp -a --remove-destination data/gcc/g++ ${name}/rootfs/usr/bin/g++
 
 ./finalizePackage.sh "${name}" "${archpkg}"
