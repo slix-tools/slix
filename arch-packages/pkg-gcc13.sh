9da54ba8ab5f5d5f9b1e5a2a67ba106dd642f9a7
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-17 18:22:18 +0200

    rename gcc to gcc13

--- arch-packages/pkg-gcc13.sh
+++ arch-packages/pkg-gcc13.sh
@@ -1,10 +1,10 @@
-name=gcc
-archpkg=${name}
+name=gcc13
+archpkg=gcc
 deps="slix-ld gcc-libs binutils libmpc zstd libisl"
 
-./preparePackage.sh "${name}" "${deps}"
+./preparePackage.sh "${name}" "${archpkg}" "${deps}"
 
 cp --remove-destination data/gcc/gcc ${name}/rootfs/usr/bin/gcc
 cp --remove-destination data/gcc/g++ ${name}/rootfs/usr/bin/g++
 
-./finalizePackage.sh "${name}"
+./finalizePackage.sh "${name}" "${archpkg}"
