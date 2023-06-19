fac31588bdc7a39db8957223f26cecb67b0f93b4
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-19 22:41:53 +0200

    remove glibc dependencies

--- arch-packages/pkg-glibc.sh
+++ arch-packages/pkg-glibc.sh
@@ -1,4 +1,14 @@
 name=glibc
 archpkg=${name}
-deps="slix-ld linux-api-headers tzdata filesystem"
-source pkg.sh
+defaultcmd=""
+deps="slix-ld"
+
+if [ -e "allreadyBuild.txt" ] && [ $(cat allreadyBuild.txt | grep "^${archpkg}$" | wc -l) -gt 0 ]; then
+    exit 0
+fi
+./preparePackage.sh "${name}" "${archpkg}" "${deps}"
+
+rm -r ${name}/rootfs/etc
+rm -r ${name}/rootfs/var
+
+./finalizePackage.sh "${name}" "${archpkg}" "${defaultcmd}"
