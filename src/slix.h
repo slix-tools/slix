36d15d16ac24ee7dff73ff9c0172adec6149bf4e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-20 22:57:52 +0200

    feat: allow -v verbose output

--- src/slix.h
+++ src/slix.h
@@ -2,6 +2,6 @@
 
 #include <clice/clice.h>
 
-inline auto cliVerbose = clice::Argument{ .args   = {"--verbose"},
+inline auto cliVerbose = clice::Argument{ .args   = {"--verbose", "-v"},
                                           .desc   = "detailed description of what is happening",
 };
