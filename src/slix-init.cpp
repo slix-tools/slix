c8f96e6c95070fc2ec44252fac6b8b7d965c719d
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-21 18:00:22 +0200

    feat: print message about adding a line to ~/.{bash,zsh}rc

--- src/slix-init.cpp
+++ src/slix-init.cpp
@@ -3,9 +3,10 @@
 #include "UpstreamConfig.h"
 
 #include <clice/clice.h>
-#include <stdexcept>
+#include <fmt/color.h>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 
 namespace {
 void app();
@@ -19,20 +20,28 @@ auto cliReset = clice::Argument { .args = "--reset",
 
 void app() {
     auto path_upstreams = getUpstreamsPath();
-    if (exists(path_upstreams) and !cliReset) {
-        fmt::print("slix seems to be initialized, abort\n");
-        return;
+    if (!exists(path_upstreams) or !cliReset) {
+        std::filesystem::remove_all(path_upstreams);
+        std::filesystem::create_directories(path_upstreams);
+        auto ofs = std::ofstream{path_upstreams / "slix-tools.de.conf"};
+        ofs << "path=https://slix-tools.de/packages/\ntype=https\n";
+        ofs.close();
+        // if reset, also remove all packages
+        if (cliReset) {
+            auto slixStatePath = getSlixStatePath();
+            std::filesystem::remove_all(slixStatePath / "packages");
+            std::filesystem::create_directories(slixStatePath / "packages");
+        }
     }
-    std::filesystem::remove_all(path_upstreams);
-    std::filesystem::create_directories(path_upstreams);
-    auto ofs = std::ofstream{path_upstreams / "slix-tools.de.conf"};
-    ofs << "path=https://slix-tools.de/packages/\ntype=https\n";
-    ofs.close();
-    // if reset, also remove all packages
-    if (cliReset) {
-        auto slixStatePath = getSlixStatePath();
-        std::filesystem::remove_all(slixStatePath / "packages");
-        std::filesystem::create_directories(slixStatePath / "packages");
+
+    auto ppid       = getppid();
+    auto parentPath = std::filesystem::canonical(fmt::format("/proc/{}/exe", ppid));
+    auto selfPath   = std::filesystem::canonical(fmt::format("/proc/self/exe"));
+    auto slixRoot   = selfPath.parent_path().parent_path().parent_path();
+    if (parentPath.filename() == "zsh") {
+        fmt::print("add `{}` to your .zshrc\n", fmt::format(fg(fmt::color::cyan), "source {}", canonical(slixRoot/"../activate")));
+    } else if (parentPath.filename() == "bash") {
+        fmt::print("add `{}` to your .bashrc\n", fmt::format(fg(fmt::color::cyan), "source {}", canonical(slixRoot/"../activate")));
     }
 }
 
