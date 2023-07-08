0368c0f245ca3e6e1f0fceb0807f4b1461df5392
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-07-08 14:52:26 +0200

    feat: additionally add ld-linux-x86-64.so.2

--- create-slix-ld.sh
+++ create-slix-ld.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
 ./build.sh
 mkdir -p slix-ld-package/rootfs/usr/bin
 cp build/bin/slix-ld slix-ld-package/rootfs/usr/bin
+cp -a /usr/lib/ld-linux-x86-64.so.2 slix-ld-package/rootfs/usr/lib
 ./build/bin/slix archive --input slix-ld-package --output slix-ld.gar
--- populate-slix-bootstrap.sh
+++ populate-slix-bootstrap.sh
@@ -15,19 +15,42 @@ sleep 1
 cp -ar slix-fs/etc slix-fs/usr rootfs
 (
     cd rootfs/usr
-    rm lib/libgo.so.22.0.0 \
-       lib/libgphobos.so.4.0.0 \
+    rm \
+       lib/libasan.so \
+       lib/libasan.so.8 \
        lib/libasan.so.8.0.0 \
-       lib/libgfortran.so.5.0.0 \
-       lib/libtsan.so.2.0.0 \
        lib/libgdruntime.so.4.0.0 \
-       lib/liblsan.so.0.0.0 \
-       lib/libubsan.so.1.0.0 \
+       lib/libgdruntime.so.4 \
+       lib/libgdruntime.so \
+       lib/libgfortran.so.5.0.0 \
+       lib/libgfortran.so.5 \
+       lib/libgfortran.so \
        lib/libgomp.so.1.0.0 \
+       lib/libgomp.so.1 \
+       lib/libgomp.so \
+       lib/libgo.so.22.0.0 \
+       lib/libgo.so.22 \
+       lib/libgo.so \
+       lib/libgphobos.so.4.0.0 \
+       lib/libgphobos.so.4 \
+       lib/libgphobos.so \
        lib/libitm.so.1.0.0 \
-       lib/libquadmath.so.0.0.0
+       lib/libitm.so.1 \
+       lib/libitm.so \
+       lib/liblsan.so.0.0.0 \
+       lib/liblsan.so.0 \
+       lib/liblsan.so \
+       lib/libquadmath.so.0.0.0 \
+       lib/libquadmath.so.0 \
+       lib/libquadmath.so \
+       lib/libtsan.so.2.0.0 \
+       lib/libtsan.so.2 \
+       lib/libtsan.so \
+       lib/libubsan.so.1.0.0 \
+       lib/libubsan.so.1 \
+       lib/libubsan.so
     rm -r lib/{pkgconfig,cmake}
-    rm -r share/{man,doc,fish,locale,zsh}
+    rm -r share/{man,doc,fish,locale,zsh,info,i18n,aclocal}
     rm -r include
 )
 cat slix-fs/slix-lock
