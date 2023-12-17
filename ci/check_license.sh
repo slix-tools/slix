77b4020c9a0eb52b5ff234e7c8ff0c714a0d7e0d
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-12-17 19:06:40 +0100

    feat: add environment file thet can check for license

---
+++ ci/check_license.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S slix run ci/check_license.slix -c bash --
+# SPDX-FileCopyrightText: 2023 S. G. Gottlieb <info.simon@gottliebtfreitag.de>
+# SPDX-License-Identifier: CC0-1.0
+
+set -Eeuo pipefail
+
+reuse lint
