0d9dbad1ddc0aeed77a720d0346cea2eab903ffc
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-12-29 17:28:52 +0100

    feat: allow additional parameter when calling check_license

--- ci/check_license.sh
+++ ci/check_license.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S slix run ci/check_license.slix -c bash --
+#!/usr/bin/env -S slix run ci/check_license.slix -c bash
 # SPDX-FileCopyrightText: 2023 S. G. Gottlieb <info.simon@gottliebtfreitag.de>
 # SPDX-License-Identifier: CC0-1.0
 
